32acfb45fb93c987b03bdca2ab7f060011ddb070
Robert Sendys <s29640@pjwstk.edu.pl>
2026-04-09 23:21:08 +0200

    Uprość konfigurację rabatów dla TeamSize

--- LegacyRenewalApp/Discounts/TeamSizeDiscountPolicy.cs
+++ LegacyRenewalApp/Discounts/TeamSizeDiscountPolicy.cs
@@ -1,29 +1,28 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace LegacyRenewalApp.Discounts
 {
     public class TeamSizeDiscountPolicy : IDiscountPolicy
     {
+        private static readonly (int MinSeats, decimal Rate, string Note)[] Rules =
+        {
+            (50, 0.12m, "large team discount"),
+            (20, 0.08m, "medium team discount"),
+            (10, 0.04m, "small team discount")
+        };
+
         public DiscountPolicyResult Apply(DiscountCalculationContext context)
         {
-            decimal discountAmount = 0m;
-            var notes = new List<string>();
+            var rule = Rules.FirstOrDefault(r => context.SeatCount >= r.MinSeats);
 
-            if (context.SeatCount >= 50)
-            {
-                discountAmount += context.BaseAmount * 0.12m;
-                notes.Add("large team discount");
-            }
-            else if (context.SeatCount >= 20)
-            {
-                discountAmount += context.BaseAmount * 0.08m;
-                notes.Add("medium team discount");
-            }
-            else if (context.SeatCount >= 10)
-            {
-                discountAmount += context.BaseAmount * 0.04m;
-                notes.Add("small team discount");
-            }
+            var discountAmount = rule == default
+                ? 0m
+                : context.BaseAmount * rule.Rate;
+
+            var notes = rule == default
+                ? new List<string>()
+                : new List<string> { rule.Note };
 
             return new DiscountPolicyResult
             {
